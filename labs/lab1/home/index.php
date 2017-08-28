9ed78dd823df382fa7d5cd25f5579a6cbb8bdd72
Erin Engle <eengle@csumb.edu>
2017-08-28 18:19:15 +0000

    adding nav links

--- labs/lab1/home/index.php
+++ labs/lab1/home/index.php
@@ -8,5 +8,11 @@
     </head>
     <body>
 Home Page
+
+
+<h4>Links to other pages:</h4>
+<a href="labs/lab1/home/index.php">About Page</a>
+<a href="labs/lab1/about/index.php">About Page</a>
+<a href="labs/lab1/contact/index.php">Contact Page</a>
     </body>
 </html>
