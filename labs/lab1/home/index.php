2b9da6a0408e1cacdd0bfaedce7a1c67ad8c4426
Erin Engle <eengle@csumb.edu>
2017-08-28 18:31:38 +0000

    fixing links

--- labs/lab1/home/index.php
+++ labs/lab1/home/index.php
@@ -11,8 +11,8 @@ Home Page
 
 
 <h4>Links to other pages:</h4>
-<a href="index.php">About Page</a>
-<a href="./about/index.php">About Page</a>
-<a href="./contact/index.php">Contact Page</a>
+<a href="index.php">Home Page</a>
+<a href="../about/index.php">About Page</a>
+<a href="../contact/index.php">Contact Page</a>
     </body>
 </html>
