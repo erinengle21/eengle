2d171a91ec4482dce5644a1189acea0222a0b988
Erin Engle <eengle@csumb.edu>
2017-09-19 16:44:32 +0000

    index file for hw2

---
+++ hw/hw2/inc/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+//it will be array.push each time a player wins a round.
+//create array for human for points
+//create array for computer for points
+//human will display one random
+//computer will display one random
+//
+
+function displayItem($humPlay, $comPlay){
+$gameArray = array("rock", "paper", "scissors");
+ 
+    echo "<img  src='img/" . $gameArray[rand(0,2)] . ".jpg' alt=' $gameArray' height='150px;'/>";
+
+}
+
+
+displayItem($humPlay, $comPlay);
+
+
+
+
+playGame($humPlay, $comPlay);
+
+function displayScore($humPlay, $comPlay){
+   
+
+
+
+$humScore = array("");
+$comScore = array("");
+
+print_r($humScore);
+print_r($comScore);
+if ($humPlay == rock && $comPlay == scissors) {
+// array_push($humScore,"X"); //human gets points
+
+}
+else if ($humPlay == rock && $comPlay == paper) {
+array_push($comScore,"X"); //computer gets points
+
+}
+else if ($humPlay == scissors && $comPlay == paper) {
+array_push($humScore,"X"); //human gets point
+
+}
+
+}
+function playGame($humPlay, $comPlay){
+if (count($comScore)  > 2){
+echo "Computer Wins!";
+}
+else if ( count($humScore) > 2) {
+echo "Human Wins!";
+}
+else {
+echo "It's a tie! Try again.";
+}
+
+   for ($i = 1; $i < 2; $i++){ //Without this for loop, images would not display.
+            ${"randomValue" . $i} = rand (1, 3);
+            displayItem(${"randomValue" . $i}, $i);
+        }
+
+
+}
+
+?>
---
+++ hw/hw2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="utf-8"/> 
+        <title> Homework 2: Rock Paper Scissors </title>
+         <link  href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <h1> Rock, Paper, Scissors,</h1>
+   <div id= "Computer">Human</div>
+    <div id= "human">Computer</div>
+ 
+<img src="img/scissor.jpg"/>
+<div id="main">
+        <?php 
+       include 'inc/functions.php';
+        ?>
+        <form> 
+<input type="submit" value="Shoot!"/>
+</form>
+
+</div>
+    </body>
+</html>
