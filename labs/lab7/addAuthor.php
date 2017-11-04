ae03abef867ebe0108fb7258ed423d2c70988a92
Erin Engle <eengle@csumb.edu>
2017-11-04 23:12:44 +0000

    more changes

--- labs/lab7/addAuthor.php
+++ labs/lab7/addAuthor.php
@@ -1,72 +1,72 @@
 <?php
 
-if (isset($_GET['addForm'])){//checks if form was submitted
-    include '../../dbConnection.php';
-$conn = getDatabaseConnection();
+ 
+ if (isset($_GET['addForm'])) { //checks if form was submitted
+     
+     include '../../dbConnection.php';
+     $conn = getDatabaseConnection();
+     
+     //echo "Form was submitted!";
+     $sql = "INSERT INTO q_author
+            (firstName, lastName, gender, dob, dod, profession, country, picture, biography)
+            VALUES 
+            (:fName, :lName, :gender, :dob, :dod, :profession, :country, :picture, :biography)";
+     $np = array();
+     $np[":fName"]  = $_GET['firstName'];
+     $np[":lName"]  = $_GET['lastName'];
+     $np[":gender"]  = $_GET['gender'];
+     $np[":dob"]  = $_GET['dob'];
+     $np[":dod"]  = $_GET['dod'];
+     $np[":profession"]  = $_GET['profession'];
+     $np[":country"]  = $_GET['country'];
+     $np[":picture"]  = $_GET['picture'];
+     $np[":biography"]  = $_GET['biography'];
+     
+     $stmt = $conn->prepare($sql);
+     $stmt->execute($np);
+     
+     echo "Author added!";
+     
+ }
 
-    echo "Form was submitted!"; 
-    
-    $sql = "INSERT INTO `q_author` ( firstName, lastName,
-    gender, dob, dod, 
-    profession, country,
-    picture, biography) VALUES 
-    (:fName, :lName, :gender, :dob, :dod, :profession, :country, :picture, :biograhpy)";
 
-
-$np = array();
-$np[":fName"] = $_GET['firstName'];
-$np[":lName"] = $_GET['lastName'];
-$np[":gender"] = $_GET['gender'];
-$np[":dob"] = $_GET['dob'];
-$np[":dod"] = $_GET['dod'];
-$np[":country"] = $_GET['country'];
-$np[":profession"] = $_GET['profession'];
-$np[":picture"] = $_GET['picture'];
-$np[":biograhpy"] = $_GET['biograhpy'];
-$stmt = $conn->prepare($sql);
-$stmt->execute($np);
-
-    echo "Author Added!";
-}
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Add new authors </title>
-             <link rel="stylesheet" type="text/css" href="css/styles.css" />
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-   
+        <title> Adding New Author</title>
     </head>
     <body>
-<h1>Add new author</h1>
-<fieldset>
-    <legend>Adding New Author</legend>
-      <form action="admin.php">
+
+        <h1> Add New Author </h1>
+        
+        <fieldset>
+            
+            <legend> Adding New Author </legend>
+       <button> <a href="admin.php">List of Authors</a></button>
+            <form>
+                
                 First Name: <input type="text" name="firstName"/> <br />
-                Last Name: <input type="text" name="lastName"/> <br />  
-                Gender:  
-             <label>Male   <input type="radio" id="genderM" name="gender" id="male" value="M" ></label>
-                <label>Female  <input type="radio" id="genderF" name="gender" id="female" value="F" ></label><br />
-                Country: 
-               <select name="country">
-                    <option value="">Select a Country</option>
-                   <option>France</option>
-                   <option>England</option>
-                   <option>USA</option>
-                   <option>Germany</option>
-                   <option>China</option>
-                   <option>Poland</option>
-                   <option>Other</option>
-                </select>
-                Profession: <input type="text" name="profession"/> <br />
-                Day of Birth: <input type="date" name="dob"/> <br />
-                Day of Death: <input type="date" name="dod"/> <br />
-                Picture:<input type="text" name="picture"/> <br />
-                Biography:<textarea name="biography" cols="20" rows="5"></textarea>
-                <input type="submit" value"Add Author" name="addForm">
-                </form>
-</fieldset>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+                Last Name: <input type="text" name="lastName"/> <br />
+                Gender: <input type="radio" name="gender" value="F"
+                            id="genderF"/><label for="genderF"></label>Female
+                         <input type="radio" name="gender" value="M"
+                            id="genderM"/><label for="genderF"></label>Male <br />   
+                Birth Date: <input type="date" name="dob"/><br /> 
+                Death Date: <input type="date" name="dod"/><br /> 
+                Profession: <input type="text" name="profession"/><br /> 
+                Country: <select name="country">
+                            <option>USA</option>
+                            <option>Germany</option>
+                            <option>China</option>
+                            <option>India</option>
+                        </select><br /> 
+                Picture URL: <input type="text" name="picture"/>   <br>
+                Biography: <br /> <textarea name="biography" cols="55" rows="5"></textarea><br>
+                <input type="submit" value="Add Author" name="addForm">
+            </form>
+            
+        </fieldset>
     </body>
 </html>
