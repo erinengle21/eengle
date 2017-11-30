c259a5af6150f2af617c31e52744479542c71131
Erin Engle <eengle@csumb.edu>
2017-11-30 07:55:16 +0000

    more chances to sql

--- hw/hw5/hw5Ajax.php
+++ hw/hw5/hw5Ajax.php
@@ -1,50 +1,37 @@
 <?php
 session_start();
-//Will be checking score in this, posting new score,
+
 
 include 'dbConnection.php';
-// include 'index.php';
+
 
 $conn = getDatabaseConnection();
-// //make this an entire function just for log in, then call function in other file..dont forget to make $conn global.
-//     $sql = "SELECT username 
-//             FROM bq_user
-//             WHERE username = :username";
-            
-//     $stmt = $conn -> prepare ($sql);
-//     $stmt -> execute( array(":username"=>$_GET['username']) );
-//     $record = $stmt -> fetch(PDO::FETCH_ASSOC);
-    
-//     //print_r($record);
-    
-//     echo json_encode($record);
 
-
-// $_SESSION['username'];
   
      
      
 function averageScore(){
     global $conn, $np;
-      $sql = "SELECT AVG(score) FROM bq_user 
+      $sql = "SELECT AVG(score), COUNT(username) FROM bq_user 
       WHERE username = :username";
     //   $_SESSION['username'];
 
     $np[":username"]  = $_SESSION['username'];
- 
-    
-     
+
+  
      $stmt = $conn->prepare($sql);
      $stmt->execute($np);
     
          $records = $stmt->fetchAll(PDO::FETCH_ASSOC);  //retrieves all records;
       foreach ($records as $record){
-          echo "<h1 style='color: red'>" . $record["AVG(score)"] . "</h1>";
+          echo "<h1 style='color: red'> Average: " . $record["AVG(score)"] . " 
+          and Count: " .   $record['COUNT(username)'] ."</h1>";
       }
 
 }
 averageScore();
-//   echo "<h1 style='color:red'>" . TESTING . "</h1>";
+
+
 
     $sql = "INSERT INTO bq_user
             (score, username)
