bdbb02cfee5ac7980f14565784985e796e981c8b
Erin Engle <eengle@csumb.edu>
2017-12-01 01:32:09 +0000

    more attempts at resize

--- labs/lab10/index.php
+++ labs/lab10/index.php
@@ -15,8 +15,10 @@ move_uploaded_file($_FILES['myFile']['tmp_name'], "gallery/" . $_FILES['myFile']
 $files = scandir("gallery/", 1);
 print_r($files);
 for ($i = 0; $i < count($files)-2; $i++){
-    echo "<img src='gallery/" . $files[$i] . "'>";
-}
+    echo "<div id='gallery'>";
+    echo "<img class='images' src='gallery/" . $files[$i] . "'>";
+    echo "</div>";
+} 
 }//endIf
 
 //if file size is more than 1 million bytes, error that file is to big
@@ -32,10 +34,7 @@ for ($i = 0; $i < count($files)-2; $i++){
     <head>
         <title>Lab 10 </title>
          <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-        <style type="text/css">
-            
-
-        </style>
+      <link rel="stylesheet" type="text/css" href="css/styles.css" />
     </head>
     <body>
 <h1> Photo Gallery </h1>
@@ -46,15 +45,65 @@ for ($i = 0; $i < count($files)-2; $i++){
     
 </form>
 <br />
-<?=displayImages()?>
+
+<!--<img id="mrbean" src="http://2.bp.blogspot.com/-C6KY8tsc8Fw/T-SVFnncxjI/AAAAAAAAANw/FMiNzA8Zecw/s640/mr.bean.jpg" width="50" height="50" />-->
+
+<!--<input type="button" value="Increase image size" />-->
 <script>
-    $(function ()
-{
-    $('img').on('click', function ()
-    {
-        $(this).width(1000);
-    });
-});
+// $(".images").click(function(){
+//   $(this).animate({ 
+//     width: 400,
+//     height: 400
+//   }, 3000 );
+// });
+// // JavaScript
+// $(".images").click(function() {
+//     var img = $("#mrbean");
+
+//     if (img.width() < 200)
+//     {
+//         img.animate({width: "200px", height: "200px"}, 1000);
+//     }
+//     else 
+//     {
+//         img.animate({width: img.attr("width"), height: img.attr("height")}, 1000);
+//     }
+// });
 </script>
+<?=displayImages()?>
+ <script>
+//     $(function ()
+// {
+//     var img = $("img");
+//     $('.images').on('click', function (){
+//     // {
+//     //     // $(this).width(1000);
+//     //     $(this).animate(1000);
+    
+//     if (img.width() < 2000)
+//     {
+//         img.animate({width: "200px", height: "200px"}, 1000);
+//     }
+//     else if (img.width() >= 200)
+//     {
+//         // img.animate({width: img.attr("width"), height: img.attr("height")}, 1000);
+//         img.animate({width: "200px", height: "200px"}, 1000);
+//     }
+   
+// });
+    if ($(e.target).hasClass('expanded')) {
+    $(".images").removeClass('expanded').stop().animate({
+        width: 280,
+        height: 187
+    }, 200);
+} else {
+    $('#gallery').find('img').fadeTo(0, 1);
+    $(".images").addClass('expanded').stop().animate({
+        width: 800,
+        height: 533
+    }, 200);
+}
+// });
+ </script>
     </body>
 </html>
